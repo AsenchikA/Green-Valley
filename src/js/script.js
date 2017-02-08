ddac45c9cc4b638b3cf05b68c5b628a159e2e407
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-02-08 22:59:19 +0400

    Простилизован промо-блок

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
 
-var toggler = document.getElementById("toggler");
-toggler.onclick = function(e){
-  e.preventDefault();
-  // toggler.classList.toggle("toggler--close");
-  document.getElementById('main-nav').classList.toggle('main-nav--visible');
-}
+  var toggler = document.getElementById("toggler");
+  toggler.onclick = function(e){
+    e.preventDefault();
+    // toggler.classList.toggle("toggler--close");
+    document.getElementById('main-nav').classList.toggle('main-nav--visible');
+  }
 
-});
+  $("#promo-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false
+  })
+
+  $('#promo-thumbs').owlCarousel({
+    loop: true,
+    dots:false,
+    center: true,
+    items: 7,
+  })
+
+  $('#gallery-thumbs a').on('click', function(event){
+    event.preventDefault();
+    var mainImageUrl = $(this).attr('href');
+    $('#gallery-main-img').attr('src',mainImageUrl);
+  });
+})
