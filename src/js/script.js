421b538f8f61a2a88b264eb3727991d14bf6012e
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-02-12 22:37:12 +0400

    СТилизация главной страницы

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
     document.getElementById('main-nav').classList.toggle('main-nav--visible');
   }
 
-  $("#promo-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    dots: false
-  })
+  // $("#promo-carousel").owlCarousel({
+  //   items: 1,
+  //   loop: true,
+  //   dots: false
+  // })
 
   $('#promo-thumbs').owlCarousel({
     loop: true,
@@ -25,4 +25,40 @@ $(document).ready(function(){
     var mainImageUrl = $(this).attr('href');
     $('#gallery-main-img').attr('src',mainImageUrl);
   });
+
+  $('#promo-tabs--1').on('click', function(event){
+    event.preventDefault();
+    $('#promo-tabs--2').removeClass('promo-tabs__item--active');
+    $('#promo-tabs--1').addClass('promo-tabs__item--active');
+    $('#promo-company').removeClass('promo-slider__item--active');
+    $('#promo-family').addClass('promo-slider__item--active');
+  });
+
+  $('#promo-tabs--2').on('click', function(event){
+    event.preventDefault();
+    $('#promo-tabs--1').removeClass('promo-tabs__item--active');
+    $('#promo-tabs--2').addClass('promo-tabs__item--active');
+    $('#promo-family').removeClass('promo-slider__item--active');
+    $('#promo-company').addClass('promo-slider__item--active');
+  });
+
+  $('#reviews-carousel').owlCarousel({
+    loop: true,
+    dots:false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      }
+    }
+  })
+
+  // $('#action-carousel').slick({
+  //   // arrows: false,
+  //   // centerMode: true,
+  //   // dots: true,
+  //   // slidesToShow: 1
+  // });
 })
